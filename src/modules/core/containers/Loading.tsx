ba643e69a0189258bb208edc7f8dd331f621cf62
FreesiaBloom <agata.jasinska215@gmail.com>
2024-07-14 18:10:59 +0200

    feature add lazy loading

---
+++ src/modules/core/containers/Loading.tsx
@@ -0,0 +1,11 @@
+function Loading() {
+  return (
+    <section>
+      <div className="container">
+        <div className="row table-responsive-md">Loading ...</div>
+      </div>
+    </section>
+  );
+}
+
+export default Loading;
